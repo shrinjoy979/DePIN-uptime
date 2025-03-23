6e78279b15fd9e5b4bdef3bd119c8d8d6d56aaad
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2025-03-24 01:35:40 +0530

    add clerk and shadcn, dark mode

--- apps/api/middleware.ts
+++ apps/api/middleware.ts
@@ -1,12 +1,25 @@
-import type { Request, Response, NextFunction } from "express";
+// import type { Request, Response, NextFunction } from "express";
 
-export function authMiddleware(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  const authorization = req.headers["authorization"];
+// export function authMiddleware(
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) {
+//   const authorization = req.headers["authorization"];
 
-  req.userId = "1";
-  next();
-}
+//   req.userId = "1";
+//   next();
+// }
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
---
+++ apps/frontend/components/Appbar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+export function Appbar() {
+  return (
+    <div className="flex justify-between items-center p-4">
+      <div>DE-PIN Uptime</div>
+      <div>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
+}
